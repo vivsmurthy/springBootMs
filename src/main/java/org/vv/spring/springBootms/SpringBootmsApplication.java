2e4ece9dae07862e1f95e1913a7246b7584b359b
Vivekanandan <vivekanandan@Vedas-MacBook-Pro.local>
2019-03-21 01:42:11 -0400

    added source folder

---
+++ src/main/java/org/vv/spring/springBootms/SpringBootmsApplication.java
@@ -0,0 +1,13 @@
+package org.vv.spring.springBootms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootmsApplication.class, args);
+	}
+
+}
